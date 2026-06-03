e77e22d9604da09444e34360bf976c93c25ae7c4
Sarvesh Shivashankaraiah <pixelsaints@users.noreply.github.com>
2026-06-03 14:37:48 +0530

    robots updated

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,7 +3,7 @@ export default function robots() {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        disallow: "",
       },
     ],
     sitemap: "https://cosmedd.com/sitemap.xml",
